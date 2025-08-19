99da238ea32cc6028c4ce5008ff88a7e84cdef72
roshan-saini-159 <roshansaini159@gmail.com>
2025-08-19 13:08:35 +0530

    Added Project : Library Management

---
+++ libraryManagement.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Book{
+    private:
+        int bookId;
+        string title;
+        string author;
+        bool isIssued;
+    
+};
+
+class Library{
+    
+};
+
+int main(){
+
+    
+    
+    return 0;
+}
