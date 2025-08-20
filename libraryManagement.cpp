3dce480458e0c788cf7626e981460935e91900ef
roshan-saini-159 <roshansaini159@gmail.com>
2025-08-21 00:02:22 +0530

    Added getters in class Book and added functions structure in Class Library

--- libraryManagement.cpp
+++ libraryManagement.cpp
@@ -4,20 +4,68 @@ using namespace std;
 
 class Book{
     private:
-        int bookId;
+        int bookNo;
+        static int nextBookNo;
         string title;
         string author;
         bool isIssued;
+
+    public:
+        Book(string title,string author){
+            bookNo = nextBookNo++;
+            this->title = title;
+            this->author = author;
+            isIssued = false;
+        }
+
+        int getBookNo() const {
+            return bookNo;
+        }
+        
+        string getTitle() const (){
+            return title;
+        }
+
+        string getAuthor() const(){
+            return author;
+        }
+
+        bool getStatus(){
+            return isIssued;
+        }
     
 };
 
 class Library{
-    
+
+    private:
+
+        vector<Book>Books;
+
+    public:
+       
+   
+        void addBook(){
+
+        }
+
+        void issueBook(){
+
+        }
+
+        void showBooks(){
+
+
+        }
+
 };
+
+int Book :: nextBookNo = 1;
 
 int main(){
 
-    
+     
+
     
     return 0;
 }
